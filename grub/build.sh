453a437fba2fe2e13d2f4c0543409ec5bbd59ed6
igorkll <frerther@gmail.com>
2026-08-08 23:56:18 +0300

    Update build.sh

--- grub/build.sh
+++ grub/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-rm -rf build
-mkdir build
+# -------------- download grub
 
 rm -rf .temp
 mkdir .temp
@@ -9,6 +8,25 @@ cd .temp
 
 wget https://ftp.gnu.org/gnu/grub/grub-2.14.tar.xz
 tar -xvf grub-2.14.tar.xz
-cd grub-2.14
+
+cd ..
+
+# -------------- build official grub
+
+reset_grub_variant() {
+    local variant_name="$1"
+
+    rm -rf "$variant_name"
+    mkdir "$variant_name"
+}
+
+build_grub_target() {
+    local variant_name="$1"
+
+    cd "$variant_name"
+}
+
+reset_grub_variant "official-2.14"
 
 
+.temp/grub-2.14/configure --with-platform=efi --target=x86_64
