b64da23a6b45a474194cca9ecce67a8746803ffd
igorkll <frerther@gmail.com>
2026-08-09 00:29:16 +0300

    Update build.sh

--- grub/build.sh
+++ grub/build.sh
@@ -29,7 +29,8 @@ build_grub_target() {
     local platform="$2"
     local target="$3"
 
-    local build_path="build/${variant_name}/${target}-${platform}"
+    local dir_name="${target}-${platform}"
+    local build_path="build/${variant_name}/${dir_name}"
 
     # ---- build
 
@@ -37,17 +38,24 @@ build_grub_target() {
     cd ".${build_path}"
     ../../../.temp/grub-2.14/configure --with-platform=$platform --target=$target
     make -j$(nproc)
+    grub-mkimage -O "${dir_name}" -o "${}" -p /boot/grub biosdisk part_msdos part_gpt fat ext2 normal efi_gop efi_uga configfile
     cd ../../..
-    pwd
 
-    # ---- export build
+    # ---- export
 
     mkdir -p "${build_path}"
     cp -r ".${build_path}/grub-core/." "${build_path}/"
 }
 
 reset_grub_variant "official-2.14"
+build_grub_target "official-2.14" "pc" "x86_64"
 build_grub_target "official-2.14" "efi" "x86_64"
+build_grub_target "official-2.14" "pc" "i386"
+build_grub_target "official-2.14" "efi" "i386"
+build_grub_target "official-2.14" "pc" "arm64"
+build_grub_target "official-2.14" "efi" "arm64"
+build_grub_target "official-2.14" "pc" "arm"
+build_grub_target "official-2.14" "efi" "arm"
 
 # -------------- cleanup
 
