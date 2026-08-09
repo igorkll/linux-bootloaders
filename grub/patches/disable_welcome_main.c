53f69a1cacb60b7224df745ea0d19fb6ac92a44d
igorkll <frerther@gmail.com>
2026-08-09 16:16:08 +0300

    Update disable_welcome_main.c

--- grub/patches/disable_welcome_main.c
+++ grub/patches/disable_welcome_main.c
@@ -315,7 +315,6 @@
  
    /* First of all, initialize the machine.  */
    grub_machine_init ();
-   grub_term_setcursor (grub_console_get_current_output (), 0);
  
    grub_boot_time ("After machine init.");
  
